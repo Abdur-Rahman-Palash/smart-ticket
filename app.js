65cc1fa4eaaaa7ec78ddffc131737e8b0b5c11c3
palash0111 <aymanislammayan@gmail.com>
2024-09-21 13:08:43 +0600

    updated

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const selectedSeat = document.getElementById('selected-seat');
+const totalBooked = document.getElementById('total-booked')
+let selectedSit = [];
 function seatHandle(event){
     console.log(event.innerText);
-    selectedSeat.innerHTML += `<li class="text-base font-normal flex justify-between>
+    event.classList.add('bg-primary');
+    event.classList.add('text-white');
+    selectedSit.push(event.innerText);
+   totalBooked.innerText = selectedSit.length;
+    selectedSeat.innerHTML += `<li class="text-base font-normal flex justify-between">
     <span>${event.innerText}</span>
     <span>Economy</span>
     <span>550</span>
